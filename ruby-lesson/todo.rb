57d8edb2830f87091b270de4c16838e2cf11b3ba
千田裕耕 <kuusou3333@gmail.com>
2019-11-26 19:06:12 +0900

    todoリストに
    ・push
    ・each 文

--- ruby-lesson/todo.rb
+++ ruby-lesson/todo.rb
@@ -1,32 +1,26 @@
-
-todo_list = [{"締め切り" => "未設定","タスク" => "未設定"},{"締め切り" => "未設定","タスク" => "未設定"},{"締め切り" => "未設定","タスク" => "未設定"}]
-
-# todo_list.each do |todo|
-#   print todo["締め切り"]
-#   puts todo["タスク"]
-# end
-
-puts "モードを選択"
-puts "[show]で確認"
-puts "[add]で追加"
-print " showかaddを選択ーーー"
-mode = gets.chomp!
-
-if mode == "show"
-  todo_list.each do |todo|
-  print todo["締め切り"]
-  print "までに"
-  puts todo["タスク"]
+todo_list = []
+100.times do
+   puts "【モードを選択】"
+   puts "  [show] ToDoを確認する"
+   puts "  [add] ToDoを追加する"
+   print "  showまたはaddと入力してください→"
+   mode = gets.chomp!
+   if mode == "show"
+     puts "【ToDo確認モードを選択しました】"
+     todo_list.each do |todo|
+         print todo[:締め切り]
+         print "までに"
+         puts todo[:タスク]
+     end
+   elsif mode == "add"
+     puts "【ToDo追加モードを選択しました】"
+     print "締め切りを入力してください→"
+     deadline = gets.chomp!
+     print "タスクを入力してください→"
+     task = gets.chomp!
+     todo_list.push({締め切り: deadline,タスク: task})
+   else
+     puts "エラーです．プログラムを終わります"
+     exit
+   end
 end
-
-elsif mode == "add"
-  puts "追加します"
-  print "締め切りを入力してください"
-   todo_list [0] ["締め切り"] = gets.chomp!
-  print "タスクを入力してください"
-  todo_list [0] ["タスク"] = gets.chomp!
-else 
-  puts "エラー"
-  exit
-end
-
